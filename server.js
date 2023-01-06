81f06a14ec1593190484628eb93e67dc633e5ddb
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-01-06 15:36:37 +0530

    server.js updated

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express()
 
@@ -14,17 +17,24 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "https://myrims-frontend.onrender.com"],
+        credentials: true,
+      }
+    ));
+
+    app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes Middleware
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
 
 // Routes
 app.get("/", (req, res) => {
     res.send("Home Page");
 });
-
-const PORT = process.env.PORT || 5000;
 
 
 // Error Middleware
@@ -33,7 +43,7 @@ app.use(errorHandler);
 
 // Connect to database and start server
 
-mongoose.set("strictQuery", false);
+const PORT = process.env.PORT || 5000;
 
 mongoose
     .connect(process.env.MONGO_URI)
